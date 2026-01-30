e11148d97d2a2929d8d663f3692c414b671f4d61
William Rockwood <wrockwood@tadl.org>
2026-01-30 15:44:18 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'tadlMobile7',
+  webDir: 'www'
+};
+
+export default config;
